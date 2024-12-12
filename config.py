621eadbf9bb6369eca4862733a80002c89168af9
Shehu Ibrahim <33332955+shehu-babs@users.noreply.github.com>
2024-12-12 08:41:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config():
     domain = 'groupsix.edu'
     smtp_username=""
     smtp_password=""
-    db_host = '141.209.241.57'
-    user='shehu1i'
+    db_host = '000.000.00.00'
+    user='root'
     password='mypass'
     database ='BIS698W_29'
 
